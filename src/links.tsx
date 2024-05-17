be3901b18e20e3d47dfa2a0e2cdf3129a8e12dca
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2024-05-17 23:32:27 +0500

    edit: change link

--- src/links.tsx
+++ src/links.tsx
@@ -1,5 +1,5 @@
 // links.tsx
 const backurl =
-  'http://5.182.26.16/plesk-site-preview/backend-api.educore-org.uz/https/5.182.26.16/';
+  'http://5.182.26.16/plesk-site-preview/backend-api.educore-org.uz/https/5.182.26.16';
 
 export default backurl;
