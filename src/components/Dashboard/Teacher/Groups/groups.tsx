e5da8a8bc56c9ef05e959b1f98af0b866305db60
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2024-05-23 00:56:37 +0500

    add: add groups and make card for each

--- src/components/Dashboard/Teacher/Groups/groups.tsx
+++ src/components/Dashboard/Teacher/Groups/groups.tsx
@@ -1,5 +1,6 @@
 import { NavLink } from 'react-router-dom';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import backurl from '../../../../links';
 
 const products = [
   {
@@ -11,6 +12,24 @@ const products = [
   },
 ];
 
+const courses = [
+  {
+    id: 1,
+    name: 'SAT',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/SAT_logo_%282017%29.svg/1200px-SAT_logo_%282017%29.svg.png',
+  },
+  {
+    id: 2,
+    name: 'IELTS',
+    img: 'https://i.ytimg.com/vi/C3XwcFQmQA4/maxresdefault.jpg',
+  },
+  {
+    id: 3,
+    name: 'CAMBRIDGE',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzy0nTApZ1wq_-2k1fszMGwnyxOCda5Ltw2avzRamJLw&s',
+  },
+];
+
 export default function AllGroupsListTeacher() {
   const [selectedOption, setSelectedOption] = useState('');
   const [isOptionSelected, setIsOptionSelected] = useState(false);
@@ -18,20 +37,75 @@ export default function AllGroupsListTeacher() {
   const changeTextColor = () => {
     setIsOptionSelected(true);
   };
+
+  const [courses, setCourses] = useState([]);
+
+  const token = localStorage.getItem('TOKEN');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${backurl}/api/get/courses`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+
+        const reversed = data.Data.reverse();
+
+        // console.log(reversed);
+
+        setCourses(reversed);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <>
       <div className="right-0 top-0 mx-auto">
         <h2 className="text-2xl mb-5">
           My <span className="underline">Groups</span> List
         </h2>
-        <NavLink
+        {/* <NavLink
           to="/dashboard/teacher/add/new/groups"
           className="text-sm  py-3 px-12 bg-blue-700 text-white rounded-full hover:bg-blue-500 active:bg-blue-400"
         >
           Add new Groups
-        </NavLink>
+        </NavLink> */}
       </div>
-      <h2 className="mt-10">Choose Course</h2>
+      <div className="bg-gray-100 my-20">
+        <div className="mt-10 grid lg:grid-cols-3">
+          {courses && courses
+            ? courses.map((e) => (
+                <div
+                  className="my-20 mx-4  block p-3 bg-white"
+                  key={e.course_id}
+                >
+                  {/* <img
+                    src={`${backurl}upload/${e.image}`}
+                    alt={e.title}
+                    className="h-full w-full object-cover object-center"
+                  /> */}
+                  <h2 className="text-2xl mt-5">{e.title}</h2>
+                  <NavLink
+                    to={`/dashboard/teacher/group/${e.course_id}`}
+                    className="bg-blue-600 py-2 px-5 text-white rounded hover:bg-blue-400 mt-5 inline-block"
+                  >
+                    More
+                  </NavLink>
+                </div>
+              ))
+            : "user don't have any courses"}
+        </div>
+      </div>
+      {/* <h2 className="mt-10">Choose Course</h2> */}
       {/* <div className="mt-5 grid grid-cols-1 gap-6 sm:grid-cols-3">
         <select
           value={selectedOption}
