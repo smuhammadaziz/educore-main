457c24331e2510f1767482b1c73ead3dfbc59339
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2024-05-28 15:11:29 +0500

    add: adding views page

--- src/components/Dashboard/Student/viewCourse/view.tsx
+++ src/components/Dashboard/Student/viewCourse/view.tsx
@@ -9,30 +9,35 @@ const courses = [
   },
   {
     id: 2,
-    name: 'IELTS',
+    name: 'IELTS | General English',
     img: 'https://i.ytimg.com/vi/C3XwcFQmQA4/maxresdefault.jpg',
   },
   {
     id: 3,
-    name: 'CAMBRIDGE',
+    name: 'IGCSE',
+    img: 'https://www.dutchinternationalschools.nl/wp-content/uploads/2016/07/igcse.png',
+  },
+  {
+    id: 4,
+    name: 'AS/A LEVELS',
     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzy0nTApZ1wq_-2k1fszMGwnyxOCda5Ltw2avzRamJLw&s',
   },
 ];
 
 function ViewAllTypesCouseStudent() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-4">
       {courses.map((e: any) => {
         return (
           <NavLink
             to={`/dashboard/student/view/${e.name}`}
             key={e.id}
-            className="shadow-lg px-25 py-10 bg-white dark:bg-gray-800 dark:text-white text-black hover:shadow-2xl cursor-pointer"
+            className="rounded-lg shadow-lg px-20 py-6 bg-white dark:bg-gray-800 dark:text-white text-black hover:shadow-2xl cursor-pointer"
           >
             <img
               src={e.img}
               alt={e.name}
-              className="w-50 h-50 object-contain mb-5"
+              className="w-50 h-50 object-contain mb-5 mx-auto"
             />
             <h3 className="text-xl text-center md:text-2xl lg:text-3xl font-bold">
               {e.name}
