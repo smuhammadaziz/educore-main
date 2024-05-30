b6b983124e1899a5b9a7a870c014bf833490774a
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2024-05-31 01:43:18 +0500

    add: Adding free trial

--- src/components/Dashboard/Student/viewCourse/buyone.tsx
+++ src/components/Dashboard/Student/viewCourse/buyone.tsx
@@ -1,19 +1,17 @@
 import React, { useEffect, useState } from 'react';
-
 import { Rating } from '@material-tailwind/react';
 import { NavLink, useParams } from 'react-router-dom';
-
 import DefaultLayoutStudent from '../../../../layout/DefaultStudent';
-
 import backurl from '../../../../links';
 
 const BuyOneCourseNowStudent = () => {
   const { course_id } = useParams();
-  const [data, setData] = useState([]);
-  const [profile, setProfile] = useState([]);
+  const [data, setData] = useState({});
+  const [profile, setProfile] = useState({});
+  const token = localStorage.getItem('TOKEN');
 
   useEffect(() => {
-    async function fetchCourses() {
+    async function fetchCourseData() {
       try {
         const response = await fetch(
           `${backurl}/api/get/id/by/course/${course_id}`,
@@ -22,37 +20,16 @@ const BuyOneCourseNowStudent = () => {
           throw new Error('Network response was not ok');
         }
         const data = await response.json();
-
-        const allCourse = data.getidbycourse;
-
-        setData(allCourse);
+        setData(data.getidbycourse);
       } catch (error) {
         console.log(error);
       }
     }
-    fetchCourses();
-  }, []);
-
-  const givenRatingIntoCouse = () => {
-    if (data.rating) {
-      if (data.rating == 'very bad') {
-        return 1;
-      } else if (data.rating == 'bad') {
-        return 2;
-      } else if (data.rating == 'good') {
-        return 3;
-      } else if (data.rating == 'better') {
-        return 4;
-      } else if (data.rating == 'best') {
-        return 5;
-      }
-    }
-  };
-
-  const token = localStorage.getItem('TOKEN');
+    fetchCourseData();
+  }, [course_id]);
 
   useEffect(() => {
-    async function fetchCourses() {
+    async function fetchProfileData() {
       try {
         const response = await fetch(`${backurl}/api/profile`, {
           headers: {
@@ -63,104 +40,101 @@ const BuyOneCourseNowStudent = () => {
           throw new Error('Network response was not ok');
         }
         const data = await response.json();
-
-        const allCourse = data.Profil;
-
-        // console.log(allCourse);
-
-        setProfile(allCourse);
-
-        // console.log(allCourse);
+        setProfile(data.Profil);
       } catch (error) {
         console.log(error);
       }
     }
-    fetchCourses();
-  }, []);
+    fetchProfileData();
+  }, [token]);
+
+  const renderRating = () => {
+    switch (data.rating) {
+      case 'very bad':
+        return 1;
+      case 'bad':
+        return 2;
+      case 'good':
+        return 3;
+      case 'better':
+        return 4;
+      case 'best':
+        return 5;
+      default:
+        return 0;
+    }
+  };
 
   return (
     <DefaultLayoutStudent>
       <div className="font-sans bg-white pb-20" key={course_id}>
         <div className="p-6 lg:max-w-7xl max-w-2xl max-lg:mx-auto">
-          <div className="grid items-start grid-cols-1 lg:grid-cols-5 gap-12 mt-10">
-            <div className="lg:col-span-3 bg-gray-100 rounded-sm w-full lg:sticky top-0 text-center p-8">
+          <div className="grid flex items-center grid-cols-1 lg:grid-cols-2 gap-8 mt-10">
+            <div className="bg-gray-100 rounded-sm p-8 flex flex-col justify-center items-center">
               <img
-                src={`${backurl}upload/${
-                  data && data['image'] ? data['image'] : 'SAT | IELTS'
-                }`}
+                src={`${backurl}upload/${data.image || 'SAT | IELTS'}`}
                 alt="Product"
                 className="w-150 rounded object-cover mx-auto sm:w-200 md:w-250"
               />
-
               <hr className="border-white border-2 my-6" />
+              {/* <Rating value={renderRating()} /> */}
             </div>
 
-            <div className="lg:col-span-2">
+            <div>
               <h2 className="text-2xl font-extrabold text-gray-800">
-                Course Name:{' '}
-                {data && data['title'] ? data['title'] : 'SAT | IELTS'}
+                Course Name: {data.title || 'SAT | IELTS'}
               </h2>
-              <div className="flex flex-wrap gap-4 mt-4">
-                <p className="text-gray-800 text-xl font-extrabold text-5xl ">
-                  Price: {data && data['price'] ? data['price'] : 'SAT | IELTS'}{' '}
-                  UZS
-                </p>
-              </div>
-
-              <div className="flex space-x-2 mt-4">
-                <p className="bg-black px-4 py-2 rounded-full text-white text-xl font-extrabold text-5xl ">
-                  {/* {givenRatingIntoCouse()} */}4.2
-                </p>
-              </div>
-
+              <p className="text-gray-800 text-xl font-extrabold text-5xl mt-4">
+                Price:{' '}
+                {data.price.toLocaleString('en-US').replace(/,/g, ' ') ||
+                  'SAT | IELTS'}{' '}
+                UZS
+              </p>
               <div className="mt-8">
                 <h3 className="text-lg font-bold text-gray-800">
                   About the course
                 </h3>
-                <p className="text-md font-bold text-gray-00 mt-3">
-                  {data && data['descr'] ? data['descr'] : 'SAT | IELTS'}
+                <p className="text-md text-gray-600 mt-3">
+                  {data.descr || 'SAT | IELTS'}
                 </p>
-                <p className="text-sm font-bold text-gray-00 mt-3">
-                  Period:{' '}
-                  {data && data['period'] ? data['period'] : 'SAT | IELTS'}{' '}
-                  months
+                <p className="text-sm text-gray-600 mt-3">
+                  Period: {data.period || 'SAT | IELTS'} months
                 </p>
               </div>
               <div className="mt-8">
                 <h3 className="text-lg font-bold text-gray-800">
                   About the Course Teacher
                 </h3>
-                <p className="text-md font-bold text-gray-00 mt-3">
-                  {data && data['name'] ? data['name'] : 'SAT | IELTS'}{' '}
-                  {data && data['l_name'] ? data['l_name'] : 'SAT | IELTS'}
+                <p className="text-md text-gray-600 mt-3">
+                  {`${data.name || 'SAT | IELTS'} ${
+                    data.l_name || 'SAT | IELTS'
+                  }`}
                 </p>
-                <p className="text-sm font-bold text-gray-00 mt-3">
-                  Email: {data && data['email'] ? data['email'] : 'SAT | IELTS'}
+                <p className="text-sm text-gray-600 mt-3">
+                  Email: {data.email || 'SAT | IELTS'}
                 </p>
-                <p className="text-sm font-bold text-gray-00 mt-3">
-                  Phone number:{' '}
-                  {data && data['phone'] ? data['phone'] : 'SAT | IELTS'}
+                <p className="text-sm text-gray-600 mt-3">
+                  Phone number: {data.phone || 'SAT | IELTS'}
                 </p>
               </div>
               <div className="mt-8 flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4">
-                {profile.group_id ? (
+                {!profile.group_id && (
                   <NavLink
                     to={`/dashboard/student/courses/buy/${course_id}`}
                     className="inline-flex items-center justify-center rounded-full bg-primary py-3 px-9 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10"
                   >
                     Buy now
                   </NavLink>
-                ) : null}
-                {profile.free_trial ? (
-                  <div>
-                    <NavLink
-                      to={`/dashboard/student/courses/use/free/${course_id}`}
-                      className="inline-flex items-center justify-center rounded-full bg-meta-3 py-3 px-9 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10"
-                    >
-                      Use Free Trial
-                    </NavLink>
-                  </div>
-                ) : null}
+                )}
+
+                {profile.free_trial && (
+                  <NavLink
+                    to={`/dashboard/student/courses/use/free/${course_id}`}
+                    className="inline-flex items-center justify-center rounded-full bg-meta-3 py-3 px-9 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10"
+                  >
+                    Use Free Trial
+                  </NavLink>
+                )}
               </div>
             </div>
           </div>
