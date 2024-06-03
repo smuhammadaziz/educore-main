080faabe6050d83f64f6db20b13cc6e2e8c505a2
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2024-06-03 16:37:19 +0500

    add: adding new errors

--- src/components/Dashboard/Teacher/Groups/addGroup.tsx
+++ src/components/Dashboard/Teacher/Groups/addGroup.tsx
@@ -116,7 +116,7 @@ function AddNewGroupTeacher() {
               </label>
               <input
                 type="text"
-                placeholder="Enter your main subject"
+                placeholder="Enter your maximum student limit"
                 className="w-full bg-white rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                 value={main}
                 onChange={(e) => setMain(e.target.value)}
