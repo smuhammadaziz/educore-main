199f4eb4d5d792a487ea3769b1da6ebf77c132fb
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2024-06-21 01:09:42 +0500

    edit: changing home page of teacher dashboard

--- src/pages/Role/Teacher/teacher.tsx
+++ src/pages/Role/Teacher/teacher.tsx
@@ -3,6 +3,12 @@ import React from 'react';
 import DefaultLayoutTeacher from '../../../layout/DefaultTeacher';
 import { NavLink } from 'react-router-dom';
 
+import { MdSubject } from 'react-icons/md';
+import { MdGroups2 } from 'react-icons/md';
+import { MdPlayLesson } from 'react-icons/md';
+import { MdMapsHomeWork } from 'react-icons/md';
+import { PiStudentFill } from 'react-icons/pi';
+
 const Teacher: React.FC = () => {
   return (
     <DefaultLayoutTeacher>
@@ -13,38 +19,56 @@ const Teacher: React.FC = () => {
       <div className="mt-10 grid gap-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 dark:text-white">
         <NavLink
           to="/dashboard/teacher/my/courses"
-          className="dark:bg-black dark:border-stone-500 dark:border-2  bg-white py-5 sm:py-10 text-lg sm:text-2xl px-5 sm:px-15 hover:underline shadow-2xl rounded-md m-2.5"
+          className="flex items-center dark:bg-black dark:border-stone-500 dark:border-2  bg-white py-5 sm:py-10 text-lg sm:text-2xl px-5 sm:px-15 hover:underline shadow-2xl rounded-md m-2.5"
         >
+          <span className="me-3">
+            <MdSubject size="30" />
+          </span>
           See all courses →
         </NavLink>
         <NavLink
           to="/dashboard/teacher/my/groups"
-          className="dark:bg-black dark:border-stone-500 dark:border-2 bg-white py-5 sm:py-10 text-lg sm:text-2xl px-5 sm:px-15 hover:underline shadow-2xl rounded-md m-2.5"
+          className="flex items-center dark:bg-black dark:border-stone-500 dark:border-2 bg-white py-5 sm:py-10 text-lg sm:text-2xl px-5 sm:px-15 hover:underline shadow-2xl rounded-md m-2.5"
         >
+          <span className="me-3">
+            <MdGroups2 size="30" />
+          </span>
           Adding new groups →
         </NavLink>
         <NavLink
           to="/dashboard/teacher/my/all/groups"
-          className="dark:bg-black dark:border-stone-500 dark:border-2 bg-white py-5 sm:py-10 text-lg sm:text-2xl px-5 sm:px-15 hover:underline shadow-2xl rounded-md m-2.5"
+          className="flex items-center dark:bg-black dark:border-stone-500 dark:border-2 bg-white py-5 sm:py-10 text-lg sm:text-2xl px-5 sm:px-15 hover:underline shadow-2xl rounded-md m-2.5"
         >
+          <span className="me-3">
+            <MdGroups2 size="30" />
+          </span>
           My all groups →
         </NavLink>
         <NavLink
           to="/dashboard/teacher/my/all/lessons"
-          className="dark:bg-black dark:border-stone-500 dark:border-2 bg-white py-5 sm:py-10 text-lg sm:text-2xl px-5 sm:px-15 hover:underline shadow-2xl rounded-md m-2.5"
+          className="flex items-center dark:bg-black dark:border-stone-500 dark:border-2 bg-white py-5 sm:py-10 text-lg sm:text-2xl px-5 sm:px-15 hover:underline shadow-2xl rounded-md m-2.5"
         >
+          <span className="me-3">
+            <MdPlayLesson size="30" />
+          </span>
           My lessons →
         </NavLink>
         <NavLink
           to="/dashboard/teacher/my/all/homeworks"
-          className="dark:bg-black dark:border-stone-500 dark:border-2 bg-white py-5 sm:py-10 text-lg sm:text-2xl px-5 sm:px-15 hover:underline shadow-2xl rounded-md m-2.5"
+          className="flex items-center dark:bg-black dark:border-stone-500 dark:border-2 bg-white py-5 sm:py-10 text-lg sm:text-2xl px-5 sm:px-15 hover:underline shadow-2xl rounded-md m-2.5"
         >
+          <span className="me-3">
+            <MdMapsHomeWork size="30" />
+          </span>
           My homeworks →
         </NavLink>
         <NavLink
           to="/dashboard/teacher/my/students"
-          className="dark:bg-black dark:border-stone-500 dark:border-2 bg-white py-5 sm:py-10 text-lg sm:text-2xl px-5 sm:px-15 hover:underline shadow-2xl rounded-md m-2.5"
+          className="flex items-center dark:bg-black dark:border-stone-500 dark:border-2 bg-white py-5 sm:py-10 text-lg sm:text-2xl px-5 sm:px-15 hover:underline shadow-2xl rounded-md m-2.5"
         >
+          <span className="me-3">
+            <PiStudentFill size="30" />
+          </span>
           My all students →
         </NavLink>
       </div>
