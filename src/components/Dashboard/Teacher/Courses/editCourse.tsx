c1d2b66eedf7cc14dae3dd40ad35568391170cdd
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2024-06-23 20:41:32 +0500

    update: changing update course page

--- src/components/Dashboard/Teacher/Courses/editCourse.tsx
+++ src/components/Dashboard/Teacher/Courses/editCourse.tsx
@@ -176,35 +176,35 @@ function EditCourseTeacher() {
         Editing <span className="underline">Course</span>
       </div>
       <form onSubmit={handleSubmit} className="dark:text-white">
-        <div className="p-6.5">
+        <div className="">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <div className="mb-4.5 px-2">
+            <div className="my-3">
               <label className="mb-2.5 block text-black dark:text-white">
                 Course name
               </label>
               <input
                 type="text"
-                placeholder="Enter your Course title"
+                placeholder="Type here ..."
                 className="w-full bg-white rounded border-[1.5px] border-stroke py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
                 required
               />
             </div>
-            <div className="mb-4.5 px-2">
+            <div className="my-3">
               <label className="mb-2.5 block text-black dark:text-white">
                 Short information about your course
               </label>
               <input
                 type="text"
-                placeholder="Enter your short information"
+                placeholder="Type here ..."
                 className="w-full bg-white rounded border-[1.5px] border-stroke py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                 value={description}
                 onChange={(e) => setDescription(e.target.value)}
                 required
               />
             </div>
-            <div className="mb-4.5 px-2">
+            <div className="my-3">
               <label className="mb-2.5 block text-black dark:text-white">
                 Main Subject
               </label>
@@ -238,7 +238,7 @@ function EditCourseTeacher() {
               </select>
             </div>
             {(main === 'IGCSE' || main === 'AS/A-LEVELS') && (
-              <div className="mb-4.5 px-2">
+              <div className="my-3">
                 <label className="mb-2.5 block text-black dark:text-white">
                   Subjects
                 </label>
@@ -296,16 +296,17 @@ function EditCourseTeacher() {
                 </select>
               </div>
             )}
-            <div className="mb-4.5 px-2">
+            <div className="my-3">
               <label className="mb-2.5 block text-black dark:text-white flex flex-col sm:flex-row sm:items-center sm:justify-between">
                 <p>Price (example: 300000)</p>
                 <p className="mt-2 sm:mt-0 sm:ml-10">
-                  you get: {showprice.eightyThree} UZS
+                  you will recieve:{' '}
+                  <span className="font-bold">{showprice.eightyThree} UZS</span>
                 </p>
               </label>
               <input
                 type="text"
-                placeholder="Enter your course price. Example: 300000"
+                placeholder="Type here ..."
                 className="w-full bg-white rounded border-[1.5px] border-stroke py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                 value={cost}
                 // onChange={(e) => setCost(e.target.value)}
@@ -313,22 +314,22 @@ function EditCourseTeacher() {
                 required
               />
             </div>
-            <div className="mb-4.5 px-2">
+            <div className="my-3">
               <label className="mb-2.5 block text-black dark:text-white">
-                Period (only numbers) example: 6
+                Course period (only numbers) example: 6
               </label>
               <input
                 type="text"
-                placeholder="Enter your course period. Example: 6 (only numbers)"
+                placeholder="Type here ..."
                 className="w-full bg-white rounded border-[1.5px] border-stroke py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                 value={time}
                 onChange={(e) => setTime(e.target.value)}
                 required
               />
             </div>
-            <div className="mb-4.5 px-2">
+            <div className="my-3">
               <label className="mb-2.5 block text-black dark:text-white">
-                Photo (only one image)
+                Course image (only one allowed)
               </label>
               <input
                 type="file"
@@ -338,62 +339,57 @@ function EditCourseTeacher() {
             </div>
           </div>
 
-          <div className="flex items-center justify-end bg-gray-100">
-            <div className="">
-              <div className="flex flex-col">
-                <div className="flex flex-col sm:flex-col md:flex-row  items-center mt-10 mb-0">
-                  <input
-                    type="checkbox"
-                    id="music"
-                    name="interest"
-                    value="music"
-                    className="mb-3 md:mb-0 md:mr-3 transform scale-150"
-                    onChange={handleCheckboxChange}
-                  />
-                  <label
-                    htmlFor="music"
-                    className="text-right w-full md:w-auto"
-                  >
-                    <p className="ml-2 md:ml-0 text-right">
-                      I read and accept
-                      <a
-                        className="ml-2 text-blue-600 underline"
-                        href="https://docs.google.com/document/d/12A_fMNIn-3ryDlcEtXtPQJqN7c10pCyQcunGtbvVV5w/edit?usp=sharing"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Terms of Services
-                      </a>
-                    </p>
-                  </label>
-                </div>
-
-                <div className="flex flex-col md:flex-row justify-end mt-10 space-y-3 md:space-y-0 md:space-x-5">
-                  <button
-                    type="button"
-                    className="flex items-center justify-center w-full md:w-40 rounded bg-red-600 p-3 font-medium text-white hover:bg-red-500 transition duration-300"
-                    onClick={handleCancel}
-                  >
-                    <span className="mr-2">
-                      <MdCancel />
-                    </span>
-                    Cancel
-                  </button>
-                  <button
-                    type="submit"
-                    className={`flex items-center justify-center w-full md:w-40 rounded p-3 font-medium text-white transition duration-300 ${
-                      isChecked
-                        ? 'bg-green-600 hover:bg-green-500'
-                        : 'bg-green-200 cursor-not-allowed'
-                    }`}
-                    // disabled={!isChecked}
-                  >
-                    <span className="mr-2">
-                      <RxUpdate />
-                    </span>
-                    Update
-                  </button>
-                </div>
+          <div className="mt-5 flex items-center justify-center bg-gray-100 p-4">
+            <div className="flex flex-col w-full max-w-xs">
+              <div className="flex items-center mb-4">
+                <input
+                  type="checkbox"
+                  id="music"
+                  name="interest"
+                  value="music"
+                  className="me-3 transform scale-150"
+                  onChange={handleCheckboxChange}
+                />
+                <label htmlFor="music" className="flex-1 text-right">
+                  <p className="ml-2 text-sm sm:text-base">
+                    I read and accept
+                    <a
+                      className="ms-2 text-blue-600 underline"
+                      href="https://docs.google.com/document/d/12A_fMNIn-3ryDlcEtXtPQJqN7c10pCyQcunGtbvVV5w/edit?usp=sharing"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Terms of Services
+                    </a>
+                  </p>
+                </label>
+              </div>
+              <div className="flex flex-col md:flex-row justify-center mt-6 space-y-3 md:space-y-0 md:space-x-5">
+                <button
+                  type="submit"
+                  className={`flex items-center justify-center w-full md:w-32 rounded p-3 font-medium text-white transition duration-300 ${
+                    isChecked
+                      ? 'bg-green-600 hover:bg-green-500'
+                      : 'bg-green-300 dark:opacity-50 cursor-not-allowed'
+                  }`}
+                  disabled={!isChecked}
+                >
+                  <span className="mr-2">
+                    <RxUpdate />
+                  </span>
+                  <span className="text-sm sm:text-base">Update</span>
+                </button>
+                <NavLink
+                  to="/dashboard/teacher/my/courses"
+                  type="button"
+                  className="flex items-center justify-center w-full md:w-32 rounded bg-red-600 p-3 font-medium text-white hover:bg-red-500 transition duration-300"
+                  onClick={handleCancel}
+                >
+                  <span className="mr-2">
+                    <MdCancel />
+                  </span>
+                  <span className="text-sm sm:text-base">Cancel</span>
+                </NavLink>
               </div>
             </div>
           </div>
