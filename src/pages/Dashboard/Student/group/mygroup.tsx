377b7ccfb21bf783fdaebec885f13e4fb6f12d52
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2024-07-10 13:55:08 +0500

    edit: change some words in groups page student dashboard

--- src/pages/Dashboard/Student/group/mygroup.tsx
+++ src/pages/Dashboard/Student/group/mygroup.tsx
@@ -34,7 +34,7 @@ function GetMyGroupStudent() {
   }, []);
   return (
     <DefaultLayoutStudent>
-      <h2 className="text-3xl font-bold mx-auto text-center">My Groups List</h2>
+      <h2 className="text-3xl font-bold mx-auto text-center">My Groups</h2>
       <div className="mx-auto max-w-2xl px-0 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-0">
         <div className="grid  grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8">
           {data && data
