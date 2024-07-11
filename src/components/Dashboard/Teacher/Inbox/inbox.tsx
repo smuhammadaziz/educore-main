5cd6004451f1aa61e3fae3180fab080f649d1889
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2024-07-11 16:38:37 +0500

    add: adding one inbox page

--- src/components/Dashboard/Teacher/Inbox/inbox.tsx
+++ src/components/Dashboard/Teacher/Inbox/inbox.tsx
@@ -63,7 +63,7 @@ function InboxForTeachers() {
                 </p>
                 <NavLink
                   to={`/dashboard/teacher/inbox/${message.notef_id}`}
-                  className="text-center items-center block w-full text-white mt-5 inline-block bg-green-700 hover:bg-green-800 py-2 px-5 rounded-full"
+                  className="text-center items-center block w-full text-white mt-5 inline-block bg-green-600 hover:bg-green-800 py-2 px-5 rounded-full"
                 >
                   View
                 </NavLink>
