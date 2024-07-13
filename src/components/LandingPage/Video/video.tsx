1b6b31b7c2d0ca34db46e7afee9971c227540c6d
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2024-07-13 13:18:10 +0500

    edit: changing autoplay

--- src/components/LandingPage/Video/video.tsx
+++ src/components/LandingPage/Video/video.tsx
@@ -39,7 +39,7 @@ function VideoComponentHome() {
             <iframe
               width="100%"
               height="450px"
-              src="https://www.youtube.com/embed/JW_bGE0lYm4?autoplay=1"
+              src="https://www.youtube.com/embed/JW_bGE0lYm4?autoplay=0"
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
