6c9c0aa2df0510e1cbd75bbe6d76fa7c76127312
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2024-07-26 16:43:36 +0500

    add: adding company page

--- src/components/LandingPage/Partner/allCompanyCourse.tsx
+++ src/components/LandingPage/Partner/allCompanyCourse.tsx
@@ -6,19 +6,20 @@ import { NavLink, useParams } from 'react-router-dom';
 import content from '../../../localization/content';
 import useLang from '../../../hooks/useLang';
 
+import bgimg from '../../../images/sodiq/12logo.png';
+import logo from '../../../images/sodiq/Artboard 17 copy.png';
+import avatar from '../../../images/sodiq/profile-icon-person-user-19.png';
+
 function AllCompanyCourseLanding() {
   const [course, setCourses] = useState([]);
-
-  const [selectedOption, setSelectedOption] = useState('');
-  const [selectedOptionn, setSelectedOptionn] = useState('');
-  const [isOptionSelected, setIsOptionSelected] = useState(false);
-  const [isOptionSelectedd, setIsOptionSelectedd] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const productsPerPage = 15;
-
+  const [productsPerPage] = useState(10); // Set the number of products per page
+  const [selectedSection, setSelectedSection] = useState('main');
   const [selectedLanguage] = useLang();
-
   const { company_id } = useParams();
+
+  const [allTeachers, setAllTeachers] = useState([]);
+
   useEffect(() => {
     async function fetchCourses() {
       try {
@@ -30,59 +31,114 @@ function AllCompanyCourseLanding() {
         }
         const data = await response.json();
         const reversed = data.Company.reverse();
-        // console.log(reversed);
         setCourses(reversed);
       } catch (error) {
         console.log(error);
       }
     }
     fetchCourses();
-  }, []);
+  }, [company_id]);
 
-  const [price, setPrice] = useState(10000000);
-
-  const handleSliderChange = (event: any) => {
-    setPrice(event.target.value);
-  };
-
-  const filterCourses = (courses: any) => {
-    return courses
-      .filter((course: any) => {
-        if (selectedOptionn && selectedOptionn !== 'ALL') {
-          return course.subject === selectedOptionn;
+  useEffect(() => {
+    async function fetchCourses() {
+      try {
+        const response = await fetch(
+          `${backurl}api/get/company/teachers/${company_id}`,
+        );
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
         }
-        return true;
-      })
-      .filter((course: any) => {
-        if (selectedOption) {
-          const rating = parseFloat(selectedOption);
-          return course.rating >= rating && course.rating < rating + 0.1;
-        }
-        return true;
-      })
-      .filter((course: any) => {
-        return course.price <= price;
-      });
+        const data = await response.json();
+
+        console.log(data);
+
+        const reversed = data.Company.reverse();
+        setAllTeachers(reversed);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchCourses();
+  }, [company_id]);
+
+  // Get current courses
+  const indexOfLastCourse = currentPage * productsPerPage;
+  const indexOfFirstCourse = indexOfLastCourse - productsPerPage;
+  const currentCourses = course.slice(indexOfFirstCourse, indexOfLastCourse);
+
+  const renderContent = () => {
+    switch (selectedSection) {
+      case 'main':
+        return <div>Main content goes here</div>;
+      case 'teachers':
+        return (
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-3 mt-10 mx-auto">
+            {allTeachers && allTeachers.length > 0
+              ? allTeachers.map((product: any) => (
+                  <div
+                    // to={`/all/courses/${product.course_id}`}
+                    key={product.user_id}
+                    className="group bg-slate-100 p-3 rounded-lg"
+                  >
+                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                      <img
+                        src={
+                          product.user_image
+                            ? `${backurl}upload/${product.user_image}`
+                            : avatar
+                        }
+                        alt="course image"
+                        className="w-50 h-50 mx-auto border-4 border-fuchsia-900 object-cover rounded-full"
+                      />
+                    </div>
+                    <h3 className="text-xl mt-6 font-bold text-black uppercase">
+                      {product.name} {product.l_name}
+                    </h3>
+                  </div>
+                ))
+              : 'No courses available'}
+          </div>
+        );
+      case 'courses':
+        return (
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-3 mt-10 mx-auto">
+            {currentCourses && currentCourses.length > 0
+              ? currentCourses.map((product: any) => (
+                  <NavLink
+                    to={`/all/courses/${product.course_id}`}
+                    key={product.course_id}
+                    className="group"
+                  >
+                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                      <img
+                        src={`${backurl}upload/${product.course_image}`}
+                        alt="course image"
+                        className="w-100 h-64 object-cover"
+                      />
+                    </div>
+                    <h3 className="mt-4 text-lg font-bold text-gray-700">
+                      {product.course_title}
+                    </h3>
+                    <h3 className="text-lg font-bold text-black uppercase">
+                      {product.name} {product.l_name}
+                    </h3>
+
+                    <p className="mt-5 text-lg font-medium text-gray-900">
+                      {product.price.toLocaleString('en-US').replace(/,/g, ' ')}{' '}
+                      UZS
+                    </p>
+                  </NavLink>
+                ))
+              : 'No courses available'}
+          </div>
+        );
+      case 'contact':
+        return <div>Contact content goes here</div>;
+      default:
+        return null;
+    }
   };
 
-  const filteredCourses = filterCourses(course);
-
-  const indexOfLastProduct = currentPage * productsPerPage;
-  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-  const currentProducts = filteredCourses.slice(
-    indexOfFirstProduct,
-    indexOfLastProduct,
-  );
-
-  const paginate = (pageNumber: any) => {
-    setCurrentPage(pageNumber);
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  };
-
-  const changeTextColor = () => {
-    setIsOptionSelected(true);
-    setIsOptionSelectedd(true);
-  };
   return (
     <div>
       <header>
@@ -93,101 +149,87 @@ function AllCompanyCourseLanding() {
         <section>
           <div>
             <div className="bg-white">
-              <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-                <h2 className="mt-10 text-2xl mb-10 text-center mx-auto">
-                  {content[selectedLanguage as string].coursesPage.find}
-                </h2>
-
-                <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-3 mt-10 mx-auto">
-                  {course && course.length > 0
-                    ? course.map((product: any) => (
-                        <NavLink
-                          to={`/all/courses/${product.course_id}`}
-                          key={product.course_id}
-                          className="group"
-                        >
-                          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                            <img
-                              src={`${backurl}upload/${product.course_image}`}
-                              alt="course image"
-                              className="w-100 h-64 object-cover"
-                            />
-                          </div>
-                          <h3 className="mt-4 text-lg font-bold text-gray-700">
-                            {product.course_title}
-                          </h3>
-                          <h3 className=" text-lg font-bold text-black uppercase">
-                            {product.name} {product.l_name}
-                          </h3>
-
-                          <p className="mt-5 text-lg font-medium text-gray-900">
-                            {product.price
-                              .toLocaleString('en-US')
-                              .replace(/,/g, ' ')}{' '}
-                            UZS
-                          </p>
-                        </NavLink>
-                      ))
-                    : 'No courses available'}
+              <div className="relative w-full h-60">
+                <img
+                  src={bgimg}
+                  alt="Company Logo"
+                  className="w-full h-full object-cover"
+                />
+                <div className="absolute inset-0 flex items-center justify-center">
+                  <h1 className="text-white text-6xl opacity-50 font-bold uppercase">
+                    Sodiq Academy
+                  </h1>
                 </div>
+              </div>
+              <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 text-center">
+                <div className="mb-10 flex items-start w-2/3 mx-auto">
+                  <img
+                    src={logo}
+                    alt="Company Logo"
+                    className="w-45 h-45 rounded-full mx-auto"
+                  />
+                  <div className="text-left ms-10">
+                    <h2 className="text-4xl uppercase font-bold">
+                      Sodiq Academy
+                    </h2>
+                    <p className="mt-2 text-xl text-black">
+                      Sodiq Academy specializes in guiding and supporting
+                      students as they prepare for and pursue their bachelor's
+                      degree abroad. Our mission is to provide comprehensive
+                      assistance, ensuring students are well-equipped for
+                      success in their international higher education endeavors.
+                      Join us as we navigate the path to educational excellence
+                      together!
+                    </p>
+                  </div>
+                </div>
+
+                <div className="flex justify-center space-x-10 text-2xl mb-10 bg-slate-100 rounded-lg">
+                  <button
+                    onClick={() => setSelectedSection('main')}
+                    className={`px-4 py-2 ${
+                      selectedSection === 'main'
+                        ? 'text-black font-bold'
+                        : 'text-gray-500'
+                    }`}
+                  >
+                    Main
+                  </button>
+                  <button
+                    onClick={() => setSelectedSection('teachers')}
+                    className={`px-4 py-2 ${
+                      selectedSection === 'teachers'
+                        ? 'text-black font-bold'
+                        : 'text-gray-500'
+                    }`}
+                  >
+                    Teachers
+                  </button>
+                  <button
+                    onClick={() => setSelectedSection('courses')}
+                    className={`px-4 py-2 ${
+                      selectedSection === 'courses'
+                        ? 'text-black font-bold'
+                        : 'text-gray-500'
+                    }`}
+                  >
+                    Courses
+                  </button>
+                  <button
+                    onClick={() => setSelectedSection('contact')}
+                    className={`px-4 py-2 ${
+                      selectedSection === 'contact'
+                        ? 'text-black font-bold'
+                        : 'text-gray-500'
+                    }`}
+                  >
+                    Contact
+                  </button>
+                </div>
+
+                {renderContent()}
 
                 {/* Pagination buttons */}
-                <div className="mt-8">
-                  <nav className=" px-4 flex items-center justify-between sm:px-0">
-                    <div className="-mt-px w-0 flex-1 flex ">
-                      <button
-                        onClick={() => {
-                          paginate(currentPage - 1);
-                          window.scrollTo({ top: 0 });
-                        }}
-                        disabled={currentPage === 1}
-                        className="cursor-pointer rounded-full hover:bg-fuchsia-900 hover:text-white relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:text-gray-400"
-                      >
-                        {
-                          content[selectedLanguage as string].coursesPage
-                            .previous
-                        }
-                      </button>
-                    </div>
-                    <div className="hidden md:-mt-px md:flex">
-                      {Array.from(
-                        {
-                          length: Math.ceil(
-                            filteredCourses.length / productsPerPage,
-                          ),
-                        },
-                        (_, i) => (
-                          <button
-                            key={i}
-                            onClick={() => paginate(i + 1)}
-                            className={`cursor-pointer relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium ${
-                              currentPage === i + 1
-                                ? 'text-black border-black-900 border-2'
-                                : 'text-gray-500 hover:text-gray-400'
-                            }`}
-                          >
-                            {i + 1}
-                          </button>
-                        ),
-                      )}
-                    </div>
-                    <div className="-mt-px w-0 flex-1 flex justify-end">
-                      <button
-                        onClick={() => {
-                          paginate(currentPage + 1);
-                          window.scrollTo({ top: 0 });
-                        }}
-                        disabled={
-                          currentPage ===
-                          Math.ceil(filteredCourses.length / productsPerPage)
-                        }
-                        className="cursor-pointer rounded-full hover:bg-fuchsia-900 hover:text-white relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:text-gray-400"
-                      >
-                        {content[selectedLanguage as string].coursesPage.next}
-                      </button>
-                    </div>
-                  </nav>
-                </div>
               </div>
             </div>
           </div>
